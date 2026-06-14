c6153ac0b71e153fce9213631060a3306fe2d95c
Dave Gaunt <dgaunt48@gmail.com>
2026-06-14 12:37:28 +1000

    - Updates to test new FPGA core.

--- Source/VIA_6522_Tester/VIA_6522_Tester.c
+++ Source/VIA_6522_Tester/VIA_6522_Tester.c
@@ -17,11 +17,12 @@
 #define	VIA_REGISTER_DISPLAY_X	(20)
 #define VIA_REGISTER_DISPLAY_Y	(5)
 
-#define VIC_PAL_CLOCK       	(4433618)
-#define VIC_CPU_CLOCK			(VIC_PAL_CLOCK >> 2)
+#define VGA_CLOCK				(25000000)			/* 25Mhz VGA Dot Clock */
+//#define VIC_PAL_CLOCK       	(4433618)
+//#define VIC_CPU_CLOCK			(VIC_PAL_CLOCK >> 2)
 #define VIA_SLOW_CLOCK			(100000)			/* 100 khz Clock */
-#define VIC_SLOW_CLOCK			(400000)			/* 400 khz Clock */
-#define VIA_CLOCK				(VIC_SLOW_CLOCK)
+//#define VIC_SLOW_CLOCK		(400000)			/* 400 khz Clock */
+//#define VIA_CLOCK				(VIC_SLOW_CLOCK)
 
 enum device_pins {
 	PIN_ADDRESS_BIT0 = 0,
@@ -35,14 +36,15 @@ enum device_pins {
 	PIN_IRQ,				/* VIA 2 */
 	PIN_NMI,				/* VIA 1 */
 	PIN_IO0 = 22,			/* CS2 active low */
-	PIN_CLK = 24,			/* Clock Signal Generator */
-	PIN_S02_READ = 26,
+	PIN_CLK_VGA = 24,
+	PIN_CLK_S02,
+	PIN_S02_READ,
 	PIN_VSYNC,
 	PIN_HSYNC,
 	PIN_RED,
 	PIN_GREEN,
 	PIN_BLUE,
-	PIN_DATA_BIT0 = 32,
+	PIN_DATA_BIT0,
 	PIN_DATA_BIT1,
 	PIN_DATA_BIT2,
 	PIN_DATA_BIT3,
@@ -52,7 +54,9 @@ enum device_pins {
 	PIN_DATA_BIT7
 };
 
-static_assert(24 == PIN_CLK, "Clock must be on PIN 24!");
+static_assert(24 == PIN_CLK_VGA, "VGA Clock must be on PIN 24!");
+static_assert(25 == PIN_CLK_S02, "S02 Clock must be on PIN 25!");
+static_assert(32 == PIN_DATA_BIT0, "Data Bus must be in High Bits");
 
 typedef struct
 {
@@ -308,8 +312,11 @@ static void __not_in_flash_func(function_core1)(void)
 	PushVIARegister(VIA_REG_AUXILIARY_CONTROL, ReadVIARegister(VIA_REG_AUXILIARY_CONTROL));
  	WriteVIARegister(VIA_REG_TIMER1_L, 0x06);
 	PushVIARegister(VIA_REG_TIMER1_LATCH_L, ReadVIARegister(VIA_REG_TIMER1_LATCH_L));
- 	WriteVIARegister(VIA_REG_TIMER1_H, 0x09);
+ 	WriteVIARegister(VIA_REG_TIMER1_H, 0x05);
 	PushVIARegister(VIA_REG_TIMER1_LATCH_H, ReadVIARegister(VIA_REG_TIMER1_LATCH_H));
+
+	WriteVIARegister(VIA_REG_DATA_DIRA, 0xFF);
+	WriteVIARegister(VIA_REG_PORTA, 0xCC);
 
 	while(true)
  	{
@@ -323,9 +330,9 @@ static void __not_in_flash_func(function_core1)(void)
 
 		uUpdateRegisterDisplay++;
 
-		if ((uUpdateRegisterDisplay & 511) == 0)
+		if ((uUpdateRegisterDisplay & 255) == 0)
 		{
-			u32 uRegister = (uUpdateRegisterDisplay >> 9) & 0xF;
+			u32 uRegister = (uUpdateRegisterDisplay >> 8) & 0xF;
 			PushVIARegister(uRegister, ReadVIARegister(uRegister));
 		}
 	}
@@ -384,10 +391,13 @@ int main()
 	gpio_set_dir(PIN_S02_READ, GPIO_IN);
 
 #ifdef GENERATE_PHASE2_CLOCK
-	clock_gpio_init(PIN_CLK, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, ((float)SYS_CLK_HZ / (float)VIA_CLOCK));
+	clock_gpio_init(PIN_CLK_VGA, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, ((float)SYS_CLK_HZ / (float)VGA_CLOCK));
+	clock_gpio_init(PIN_CLK_S02, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, ((float)SYS_CLK_HZ / (float)VIA_SLOW_CLOCK));
 #elifndef GENERATE_PHASE2_CLOCK
-	gpio_init(PIN_CLK);
-	gpio_set_dir(PIN_CLK, GPIO_IN);
+	gpio_init(PIN_CLK_VGA);
+	gpio_set_dir(PIN_CLK_VGA, GPIO_IN);
+	gpio_init(PIN_CLK_S02);
+	gpio_set_dir(PIN_CLK_S02, GPIO_IN);
 #endif
 
 	vga_Init(PIN_RED, PIN_HSYNC, PIN_VSYNC);
@@ -396,11 +406,11 @@ int main()
 
 	// Draw All The Constant Text To The Screen
 	char szTempString[128];
-	vga_DrawString(VIA_REGISTER_DISPLAY_X + 7, VIA_REGISTER_DISPLAY_Y, "VIA 6522", RGB111_CYAN);
+	vga_DrawString(VIA_REGISTER_DISPLAY_X, VIA_REGISTER_DISPLAY_Y, "VIA 6522 Function Test", RGB111_GREEN);
 
 	for (u32 uRegisterIndex=0; uRegisterIndex<16; ++uRegisterIndex)
 	{
-		sprintf(szTempString, "0x%04X", 0x9110 + uRegisterIndex);
+		sprintf(szTempString, "0x%02X", uRegisterIndex);
 		vga_DrawString(VIA_REGISTER_DISPLAY_X, VIA_REGISTER_DISPLAY_Y + 2 + uRegisterIndex, szTempString, RGB111_BLUE);
 		vga_DrawString(VIA_REGISTER_DISPLAY_X + 7, VIA_REGISTER_DISPLAY_Y + 2 + uRegisterIndex, "0x", RGB111_YELLOW);
 		vga_DrawString(VIA_REGISTER_DISPLAY_X + 13, VIA_REGISTER_DISPLAY_Y + 2 + uRegisterIndex, s_aszRegisterNames[uRegisterIndex], RGB111_CYAN);
